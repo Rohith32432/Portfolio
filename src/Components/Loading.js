6737552af78fa875fd982f360fec2e09ed6d1b15
Rohith_k <2100032432cse@gmail.com>
2024-01-04 14:50:51 +0530

    frist commit

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,9 +1,27 @@
 import React from 'react'
-
-function Loading() {
+import './Styles/loader.css'
+function Loader() {
   return (
-    <>    loading</>
+    <svg width="500" height="500" viewBox="0 0 500 328" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+    <g id="top">
+        <path id="sub1" fill-rule="evenodd" clip-rule="evenodd"
+            d="M208.695 94H328.903L367 70H159V70.0404L208.695 94Z"></path>
+    </g>
+    
+
+    <g id="opp-diagnal">
+        <path id="sub3" fill-rule="evenodd" clip-rule="evenodd"
+            d="M252.025 137.53L343.145 244.467L316.101 245.206L209.032 119.552L209.204 119.465L252.025 137.53Z"></path>
+    </g>
+
+    <g id="diagnal">
+        <path id="sub2" fill-rule="evenodd" clip-rule="evenodd"
+            d="M317.056 85.8506L171.307 242.416L171.332 242.445L203.565 242.784L361.558 73.0657L316.2 87.1096L317.056 85.8506Z"></path>
+    </g>
+
+</svg>
   )
 }
 
-export default Loading
+export default Loader
